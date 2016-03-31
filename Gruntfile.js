5e574e5f594fbd8292913fc1fe1a5ef1ddd5b627
Paladox <thomasmulhall410@yahoo.com>
2016-03-31 22:12:28 +0100

    Add npm entry point
    Bug: T63632
    Change-Id: I3f1d307d8a89a1e4245918e91a1953c4c131d8d1

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+
+	grunt.initConfig( {
+		banana: {
+			all: 'i18n/'
+		},
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
