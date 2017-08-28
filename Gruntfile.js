837423cc82a43086576835c5015e3ff5c1742575
Umherirrender <umherirrender_de.wp@web.de>
2017-08-28 19:39:08 +0200

    Install SemanticMediaWiki over composer
    Needed to run automatic tests with jenkins
    
    Change-Id: I48715c8bd06ceebb3086d86f44f7fe87c9a6a383

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,9 @@ module.exports = function ( grunt ) {
 		jsonlint: {
 			all: [
 				'**/*.json',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**',
+				'!extensions/**'
 			]
 		}
 	} );
